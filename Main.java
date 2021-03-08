0b696a73d91d04affe37da8ba62c175aa977fe76
Munawar Asep <munawarasep78@gmail.com>
2021-03-08 15:33:49 +0000

    perubahan

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("okeyyy");
-  }
+public class Main {
+    
+    static String roomArray[][] = {
+        {"*", "*", "*", "*", "x"},
+        {"*", "*", "*", "*", "*"},
+        {"*", "*", "*", "*", "*"},
+        {"*", "*", "*", "x", "*"},
+    };
+
+    public static void main(String[] args) {
+        int sisaKamar = 0;
+        for(int i = 0; i < roomArray.length; i++) {
+            for(int j = 0; j < roomArray[i].length; j++) {
+                if(roomArray[i][j] == "*") {
+                    sisaKamar++;
+                } else {
+                    System.out.println("Tamu berada di Lantai " + (i + 1)  + " Kamar " + (j + 1));
+                    
+                }
+            }
+        }
+        System.out.println("Jumlah kamar yang tersedia adalah "+ sisaKamar +" kamar");
+    }
 }
